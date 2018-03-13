e82a115cd0dbd157a1f1467fe0b59ca908584738
vtming <vtming@qq.com>
2018-03-13 10:21:47 +0800

    首次提交

---
+++ app/src/main/java/com/wyf/codenotes/widget/ListWidget.java
@@ -0,0 +1,45 @@
+package com.wyf.codenotes.widget;
+
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+
+import com.wyf.codenotes.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListWidget extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_widget);
+    }
+
+
+    private List<String> bleList = new ArrayList<>();
+
+    /**
+     * adapter的几种简单写法
+     */
+    private void adapter(){
+        ArrayAdapter adapter1 = new ArrayAdapter(this,
+                android.R.layout.simple_list_item_1,
+                android.R.id.text1, bleList){
+            @Nullable
+            @Override
+            public Object getItem(int position) {
+                 String  data = "这是显示的数据";
+                return data;
+            }
+        } ;
+
+
+        ArrayAdapter adapter2 = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,
+                new String[]{"F1","F2","F3","F4","F5","F6","F7","F8",});
+    }
+}
